624190435edb5d7d360635a9fe9e0fa432b37929
admin <804159583@.com>
2017-11-08 19:31:22 +0800

    no message

---
+++ Om/Model/ModelView/M_HitchInfoView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.ModelView
+{
+   public  class M_HitchInfoView
+    {
+        public string FactorySation { get; set; }
+        public string Signal { get; set; }
+
+        public int HappenTimes { get; set; }
+
+        public int PlanTimes { get; set; }
+    }
+}
--- Om/Om/Controllers/ApiM_HitchInfoController.cs
+++ Om/Om/Controllers/ApiM_HitchInfoController.cs
@@ -11,6 +11,8 @@ using LeaRun.Utilities;
 using System.Xml;
 using Model.ModelView;
 using System.Text;
+using Quartz.Impl;
+using Quartz;
 
 namespace Om.Controllers
 {
@@ -152,7 +154,7 @@ namespace Om.Controllers
             }
             List<string> list = new List<string>();
           
-            data = re.FindTablePageBySql("SELECT sum(happentimes) as total,[FactorySation],CreateTime,Signal FROM[ElectricPower].[dbo].[M_HitchInfo] where " + strwhere + "   group by CreateTime,[FactorySation], Signal", ref jqgridparam);
+            data = re.FindTablePageBySql("SELECT sum(happentimes) as total,[FactorySation],CreateTime,Signal FROM  [M_HitchInfo] where " + strwhere + "   group by CreateTime,[FactorySation], Signal", ref jqgridparam);
             //for (int i = 0; i < data.Rows.Count; i++)
             //{
             //    list.Add(GetYuji1(data.Rows[i]["FactorySation"].ToString(), data.Rows[i]["Signal"].ToString(), data.Rows[i]["CreateTime"].ToString()));
@@ -448,9 +450,23 @@ namespace Om.Controllers
             xmldoc.SelectSingleNode("root").SelectSingleNode("daorutime").Attributes[0].Value = model.daorutime;
             xmldoc.SelectSingleNode("root").SelectSingleNode("daorudir").Attributes[0].Value = model.daorudir;
             xmldoc.SelectSingleNode("root").SelectSingleNode("daorunowdate").Attributes[0].Value = model.daorunowdate;
-            
             xmldoc.Save(path);
-            PredicSettingBll.ZiDongDaoRu(model.daorutime);
+            if (string.IsNullOrEmpty(model.daorutime))
+            {
+                var scheduler = StdSchedulerFactory.GetDefaultScheduler();
+                if (scheduler.CheckExists(new TriggerKey("trigger", "triggers")))
+                {
+                    scheduler.UnscheduleJob(new TriggerKey("trigger", "triggers"));
+
+                }
+
+
+            }
+            else
+            {
+                PredicSettingBll.ZiDongDaoRu(model.daorutime);
+
+            }
             return new Dictionary<string, object>
             {
                 { "code","1"}
